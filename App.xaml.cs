763cc7e8024c4d2447a8d04d66da7672d948458e
Siketyan <siketyan@sikeserver.com>
2017-11-03 12:15:38 +0900

    Add starting up on the right bottom on the screen

--- App.xaml.cs
+++ App.xaml.cs
@@ -1,9 +1,25 @@
-﻿namespace Twitify
+﻿using System.Windows;
+using Twitify.Windows;
+using Forms = System.Windows.Forms;
+
+namespace Twitify
 {
     /// <summary>
     /// Interaction logic for App.xaml
     /// </summary>
     public partial class App
     {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            var screen = Forms.Screen.PrimaryScreen;
+            var area = screen.WorkingArea;
+            var window = new MainWindow
+            {
+                Left = area.Right - 430,
+                Top = area.Bottom - 107
+            };
+
+            window.Show();
+        }
     }
 }
