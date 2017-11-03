f6eec93f6ea06d2b5d39efd43de0aeb8b52dec48
Siketyan <siketyan@sikeserver.com>
2017-11-03 12:31:16 +0900

    Add Spotify integration

--- Windows/MainWindow.xaml.cs
+++ Windows/MainWindow.xaml.cs
@@ -1,13 +1,40 @@
-﻿namespace Twitify.Windows
+﻿using SpotifyAPI.Local;
+using System.Windows;
+
+namespace Twitify.Windows
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
     public partial class MainWindow
     {
+        private SpotifyLocalAPI _spotify;
+
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void Initialize(object sender, RoutedEventArgs e)
+        {
+            _spotify = new SpotifyLocalAPI
+            {
+                ListenForEvents = true
+            };
+
+            _spotify.OnTrackChange += OnPlayingTrackChanged;
+            _spotify.Connect();
+        }
+
+        private void OnPlayingTrackChanged(object sender, TrackChangeEventArgs e)
+        {
+            var track = e.NewTrack;
+            Dispatcher.Invoke(() =>
+            {
+                Title.Content = track.TrackResource.Name;
+                Artist.Content = track.ArtistResource.Name;
+                Album.Content = track.AlbumResource.Name;
+            });
         }
     }
 }
